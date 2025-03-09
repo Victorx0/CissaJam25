708b58fb1709933c4477a7301e744a75e35251a8
Victorx0 <153782390+Victorx0@users.noreply.github.com>
2025-03-09 14:32:01 +1100

    add deliver energy button (blue button)
    you can press e on the blue button to deliver energy

--- My project/Assets/UnityTechnologies/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ My project/Assets/UnityTechnologies/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -8,21 +8,12 @@ using UnityEngine.InputSystem;
 
 namespace StarterAssets
 {
-    
     [RequireComponent(typeof(CharacterController))]
 #if ENABLE_INPUT_SYSTEM 
     [RequireComponent(typeof(PlayerInput))]
 #endif
     public class ThirdPersonController : MonoBehaviour
     {
-        //public float sensX;
-       //public float sensY;
-
-        //public Transform orientation;
-
-        //float xRotation;
-        //float yRotation;
-
         [Header("Player")]
         [Tooltip("Move speed of the character in m/s")]
         public float MoveSpeed = 2.0f;
@@ -168,16 +159,6 @@ namespace StarterAssets
             JumpAndGravity();
             GroundedCheck();
             Move();
-            //float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-            //float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * -sensY;
-
-            //yRotation += mouseX;
-
-            //xRotation += mouseY;
-            //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-            //transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-            //orientation.rotation = Quaternion.Euler(0, yRotation, 0);
         }
 
         private void LateUpdate()
--- My project/Assets/number_gen.cs
+++ My project/Assets/number_gen.cs
@@ -1,25 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
-public class reacter : MonoBehaviour
-{
-    public Slider slid;
-
-    public float currentTime = 0;
-
+public class number_gen : MonoBehaviour, IInteractable {
+    public reacter reactor;
+    public void Interact()
+    {
+        reactor.currentTime = 0;
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        currentTime += Time.deltaTime;
-
-        slid.value = currentTime;
-
+        
     }
 }
